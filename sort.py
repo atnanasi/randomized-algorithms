1a1e4d606b94cac1b84c177749d45abcf6008e88
Atnanasi <ys2000pro@gmail.com>
2017-07-13 09:31:14 +0900

    Fix indent

--- sort.py
+++ sort.py
@@ -1,15 +1,15 @@
 # -*- coding: utf-8 -*-
 
 def bubble_sort(A,n):
-    m=n
+    m = n
 
-    while m>1:
-        k=0
-        while k<m-1:
-            if A[k]>A[k+1]:
+    while m > 1:
+        k = 0
+        while k < m-1:
+            if A[k] > A[k+1]:
                 A[k], A[k+1] = A[k+1], A[k]
-            k=k+1
-        m=m-1
+            k = k+1
+        m = m-1
     return A
 
 if __name__ == '__main__':
